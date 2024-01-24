b96eef074d790505b3a7dfaa3692d80409161cc7
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-01-24 10:23:56 -0500

    Update lb_buildartefacto.groovy

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -2,8 +2,8 @@ package org.devops
 
 def clone(Map params){
     def branch_name = "${env.GIT_BRANCH}".split('/')[1]
-    def scmUrl = "https://github.com/JoseCorreaV/devops.git"
-    git branch: "${branch_name}", url: "${params.scmUrl}"
+    def scmUrl = 'https://github.com/JoseCorreaV/devops.git'
+    git branch: "${branch_name}", url: "${scmUrl}"
 }
 
 def install(){

503b0daa1014a31d7891db41ac4d5b79905f027c
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-01-31 11:11:37 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,6 +1,6 @@
 package org.devops
 
-def cargarDockerHub(projectGitName){
+def publicarImage(projectGitName){
     withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
         sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"

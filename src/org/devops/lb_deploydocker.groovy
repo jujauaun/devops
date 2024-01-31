c1c36874a0f3bd3c662be29f74f0dbe228917d9d
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-01-31 11:31:38 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,6 +1,7 @@
 package org.devops
 
-def call(PROJECT){
-    sh "docker run --name ${PROJECT} -p 80${env.BUILD_ID}:3000 -d laurabecerra/${PROJECT}:${env.BUILD_ID}"
-    
+def despliegueContenedor(projectGitName){
+    sh "docker pull josecorreav/react-test-jenkinsfile"
+    //sh "docker run --name ${PROJECT} -p 80${env.BUILD_ID}:3000 -d laurabecerra/${PROJECT}:${env.BUILD_ID}"
+    sh "docker run -p 5174:5174 --name ${projectGitName} -d josecorreav/${projectGitName}"
 }

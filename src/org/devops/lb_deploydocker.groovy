fa7e95b2cda28ffe7af45997493259eb4313c96e
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-01-31 11:40:02 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -3,5 +3,5 @@ package org.devops
 def despliegueContenedor(projectGitName){
     sh "docker pull react-test-jenkinsfile"
     //sh "docker run --name ${PROJECT} -p 80${env.BUILD_ID}:3000 -d laurabecerra/${PROJECT}:${env.BUILD_ID}"
-    sh "docker run -p 5174:5174 --name ${projectGitName} -d josecorreav/${projectGitName}"
+    sh "docker run -p 5174:5174 --name ${projectGitName} -d ${projectGitName}"
 }

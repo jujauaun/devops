79d095965c38029aa488eedfff4e1c6bdafc6ccc
josecorreav <josestivencorrea@gmail.com>
2024-01-31 15:49:06 -0500

    update to owasp

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -13,6 +13,7 @@ def call(){
         }
 
         stages{
+
             stage('Fase 2: Construcción de imagen en Docker Desktop') {
                 steps {
                     script {
@@ -21,6 +22,7 @@ def call(){
                     }
                 }
             }
+
             stage('Fase 2: publicar imagen a docker hub') {
                 steps {
                     script {
@@ -31,60 +33,15 @@ def call(){
                 } 
             }
 
-            /*stage('Fase 1: Proceso de construcción') {
-                steps {
-                    script {
-                        def cloneapp = new org.devops.lb_buildartefacto()
-                        cloneapp.clone()
-                        def buildapp = new org.devops.lb_buildartefacto()
-                        buildapp.install()
-                    }
-                }
-            }
-
-            stage('Fase 1: Análisis de Sonarqube'){
-                steps{
-                    script{
-                        def test = new org.devops.lb_analisissonarqube()
-                        test.runTest()
-                        def analisysSonarqube = new org.devops.lb_analisissonarqube()
-                        analisysSonarqube.analisys("${projectName}")
-                    }
-                }
-            }
- stage('Push Imagen') {
-                steps {
-                    script {
-                        def pushimage = new org.devops.push()
-                        pushimage("${PROJECT}")
-                    }
-                    
-                } 
-            }
-
-            stage('Deploy Imagen') {
+             stage('Fase 2: analisis con owasp') {
                 steps {
                     script{
-                        DOCKER_EXIST = sh(returnStdout: true, script: 'echo "$(docker ps -q --filter name=${PROJECT})"').trim()
-                        
-                        if (DOCKER_EXIST != '') {  
-                            sh "docker start ${PROJECT}"
-                        } else {
-                            def deployimage = new org.devops.deploy()
-                            deployimage("${PROJECT}")
-                        }
-                    }    
-                }                                        
-            }
-
-            stage('Escaneo de la aplicación') {
-                steps {
-                    script{
-                         def owaspscan = new org.devops.owasp()
-                        owaspscan("${PROJECT}")
+                        def owaspAnalisis = new org.devops.lb_owasp()
+                        owaspAnalisis.AnalisisOwasp("${projectName}")
                     }
                 }
-            }*/
+
         }
     }
+  }
 }

8820b01fac57688f55f31ceab098003b311fa26a
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-01-31 17:14:53 -0500

    Update lb_owasp.groovy

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -4,6 +4,6 @@ def AnalisisOwasp(projectGitName){
       //  sh "docker network connect ${env.NameNetwork} ${projectGitName}"
         sh "docker run -dt --name owasp owasp/zap2docker-stable /bin/bash"
         sh 'docker exec owasp mkdir /zap/wrk'
-        sh "docker exec owasp zap-full-scan.py -t https://example.com/ -r reportOwasp.html -I"
+        sh "docker exec owasp zap-full-scan.py -t ${env.dominio} -r reportOwasp.html -I"
         sh 'docker cp owasp:/zap/wrk/reportOwasp.html ${WORKSPACE}/reportOwasp.html' 
 }

2bc916b61abba96ec0b36372bde7f6b4b7aaaa7d
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-01-31 11:06:36 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,7 +1,9 @@
 package org.devops
 
-def publicarImage(projectGitName){
-    withDockerRegistry([ credentialsId: "josecorreav", url: "https://hub.docker.com/repository/docker/josecorreav/react-test-jenkinsfile" ]) {
-        sh "docker push josecorreav/${projectGitName}"
-        }
+def cargarDockerHub(projectGitName){
+    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+        sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
+        sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+    }
 }

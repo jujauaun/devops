2c7b64c2680f029c6c7e49009ac8fa06821e34f1
Jose Stiven Correa Valoyes <103680054+JoseCorreaV@users.noreply.github.com>
2024-02-01 15:12:48 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -7,6 +7,5 @@ def despliegueContenedor(projectGitName){
     sh """ docker run -d --name ${projectGitName} \
     --network=${env.NameNetwork} -p 5174:5174 \
     --user root josecorreav/${projectGitName}
-    
     """
 }

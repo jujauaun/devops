855e512d6dcd700725c3fa40963e678eb5d9d5e0
JuanD <145304610+jujauaun@users.noreply.github.com>
2024-05-31 12:10:32 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -4,7 +4,7 @@ def publicarImage (projectGitName) {
     withCredentials ([usernamePassword(credentialsId: "${env.TOKEN_ID}",
     passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
     sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-    sh "docker tag jujauaun/react-test jujauaun/react-test-jenkinsfile:latest"
+    sh "docker tag jujauaun/react-test-jenkinsfile jujauaun/react-test-jenkinsfile:latest"
     sh "docker push jujauaun/react-test-jenkinsfile:latest"
     }
 }

27d3e033d6ee29acca7a88a601d78b82cfbe2f68
JuanD <145304610+jujauaun@users.noreply.github.com>
2024-05-31 11:31:48 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -3,9 +3,9 @@ package org.devops
 def despliegueContenedor(projectGitName){
     //sh "docker stop ${projectGitName}"
     //sh "docker rm ${projectGitName}"
-    sh "docker pull josecorreav/react-test-jenkinsfile"
+    sh "docker pull jujauaun/react-test-jenkinsfile"
     sh """ docker run -d --name ${projectGitName} \
     --network=${env.NameNetwork} -p 5174:5174 \
-    --user root josecorreav/${projectGitName}
+    --user root jujauaun/${projectGitName}
     """
 }
